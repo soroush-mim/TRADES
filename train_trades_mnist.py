55ed65fa8fd1323ed4a873895e3561f55fc5a6cb
soroush <soroushm1377@gmail.com>
2023-04-15 11:05:34 +0330

    robust evaluation and code for plotting added to train_trades_mnist.py

--- train_trades_mnist.py
+++ train_trades_mnist.py
@@ -6,10 +6,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+from torch.autograd import Variable
 
 from models.net_mnist import *
 from models.small_cnn import *
 from trades import trades_loss
+
+from matplotlib import pyplot as plt
 
 
 parser = argparse.ArgumentParser(description='PyTorch MNIST TRADES Adversarial Training')
@@ -35,7 +38,7 @@ parser.add_argument('--beta', default=1.0,
                     help='regularization, i.e., 1/lambda in TRADES')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=100, metavar='N',
+parser.add_argument('--log-interval', type=int, default=20, metavar='N',
                     help='how many batches to wait before logging training status')
 parser.add_argument('--model-dir', default='./model-mnist-smallCNN',
                     help='directory of model for saving checkpoint')
@@ -63,6 +66,63 @@ test_loader = torch.utils.data.DataLoader(
                    transform=transforms.ToTensor()),
                    batch_size=args.test_batch_size, shuffle=False, **kwargs)
 
+
+
+def _pgd_whitebox(model,
+                  X,
+                  y,
+                  epsilon=args.epsilon,
+                  num_steps=args.num_steps,
+                  step_size=args.step_size):
+    out = model(X)
+    err = (out.data.max(1)[1] != y.data).float().sum()
+    X_pgd = Variable(X.data, requires_grad=True)
+    if args.random:
+        random_noise = torch.FloatTensor(*X_pgd.shape).uniform_(-epsilon, epsilon).to(device)
+        X_pgd = Variable(X_pgd.data + random_noise, requires_grad=True)
+
+    for _ in range(num_steps):
+        opt = optim.SGD([X_pgd], lr=1e-3)
+        opt.zero_grad()
+
+        with torch.enable_grad():
+            loss = nn.CrossEntropyLoss()(model(X_pgd), y)
+        loss.backward()
+        eta = step_size * X_pgd.grad.data.sign()
+        X_pgd = Variable(X_pgd.data + eta, requires_grad=True)
+        eta = torch.clamp(X_pgd.data - X.data, -epsilon, epsilon)
+        X_pgd = Variable(X.data + eta, requires_grad=True)
+        X_pgd = Variable(torch.clamp(X_pgd, 0, 1.0), requires_grad=True)
+
+    output = model(X_pgd)
+    err_pgd = (output.data.max(1)[1] != y.data).float().sum()
+    adv_loss = F.cross_entropy(output, y, size_average=False).item()
+    print('err pgd (white-box): ', err_pgd)
+    return err, err_pgd, adv_loss
+
+def eval_adv_test_whitebox(model, device, test_loader):
+    """
+    evaluate model by white-box attack
+    """
+    model.eval()
+    robust_err_total = 0
+    natural_err_total = 0
+    adv_loss = 0
+
+    for data, target in test_loader:
+        data, target = data.to(device), target.to(device)
+        # pgd attack
+        X, y = Variable(data, requires_grad=True), Variable(target)
+        err_natural, err_robust, batch_adv_loss = _pgd_whitebox(model, X, y)
+        robust_err_total += err_robust
+        natural_err_total += err_natural
+        adv_loss += batch_adv_loss
+    print('natural_err_total: ', natural_err_total)
+    print('robust_err_total: ', robust_err_total)
+    
+    adv_acc = 1 - (natural_err_total / len(test_loader.dataset)
+                   
+    return adv_loss, adv_acc
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
@@ -146,6 +206,11 @@ def main():
     # init model, Net() can be also used here for training
     model = SmallCNN().to(device)
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    train_losses, test_losses = [], []
+    train_accs, test_accs = [], []
+    adv_train_losses, adv_test_losses = [], []
+    adv_train_accs, adv_test_accs = [], []
+
 
     for epoch in range(1, args.epochs + 1):
         # adjust learning rate for SGD
@@ -156,9 +221,48 @@ def main():
 
         # evaluation on natural examples
         print('================================================================')
-        eval_train(model, device, train_loader)
-        eval_test(model, device, test_loader)
+        train_loss, training_accuracy = eval_train(model, device, train_loader)
+        test_loss, test_accuracy = eval_test(model, device, test_loader)
         print('================================================================')
+        adv_test_loss, adv_test_accuracy = eval_adv_test_whitebox(model, device, test_loader)
+        adv_train_loss, adv_training_accuracy = eval_adv_test_whitebox(model, device, train_loader)
+
+        train_losses.append(train_loss)
+        test_losses.append(test_loss)
+        train_accs.append(training_accuracy)
+        test_accs.append(test_accuracy)
+
+        adv_train_losses.append(adv_train_loss)
+        adv_test_losses.append(adv_test_loss)
+        adv_train_accs.append(adv_training_accuracy)
+        adv_test_accs.append(adv_test_accuracy)
+
+    
+    plt.plot(train_accs, label = 'train_acc')
+    plt.plot(test_accs, label = 'test_acc')
+    plt.legend()
+    plt.savefig('accs.png')
+    plt.clf()
+
+    plt.plot(adv_train_accs, label= 'train_acc_robust')
+    plt.plot(adv_test_accs, label = 'test_robust_accs')
+    plt.legend()
+    plt.savefig('adv accs.png')
+    plt.clf()
+
+    #plot the same for losses
+    
+    plt.plot(train_losses, label = 'train_loss')
+    plt.plot(test_losses, label = 'test_loss')
+    plt.legend()
+    plt.savefig('losses.png')
+    plt.clf()
+
+    plt.plot(adv_train_losses, label = 'train_loss_robust')
+    plt.plot(adv_test_losses, label = 'test_robust_loss')
+    plt.legend()
+    plt.savefig('adv losses.png')
+
 
         # save checkpoint
         if epoch % args.save_freq == 0:

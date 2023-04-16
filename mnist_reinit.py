2df3cee1a2b06ab48af4b01f000eb7c26fb5d712
soroush <soroushm1377@gmail.com>
2023-04-16 11:01:40 +0330

    mnist_reinit added for adversarial training on mnist with shrink and perturb

---
+++ mnist_reinit.py
@@ -0,0 +1,313 @@
+from __future__ import print_function
+import os
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+
+from models.net_mnist import *
+from models.small_cnn import *
+from trades import trades_loss
+
+from copy import deepcopy
+
+from matplotlib import pyplot as plt
+
+
+parser = argparse.ArgumentParser(description='PyTorch MNIST TRADES Adversarial Training')
+parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                    help='input batch size for training (default: 128)')
+parser.add_argument('--test-batch-size', type=int, default=128, metavar='N',
+                    help='input batch size for testing (default: 128)')
+parser.add_argument('--epochs', type=int, default=100, metavar='N',
+                    help='number of epochs to train')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate')
+parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                    help='SGD momentum')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--epsilon', default=0.3,
+                    help='perturbation')
+parser.add_argument('--num-steps', default=10,
+                    help='perturb number of steps')
+parser.add_argument('--attack-num-steps', default=40,type=int,
+                    help='perturb number of steps for attack in eval')
+parser.add_argument('--step-size', default=0.01,
+                    help='perturb step size')
+parser.add_argument('--beta', default=1.0, type=float,
+                    help='regularization, i.e., 1/lambda in TRADES')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=20, metavar='N',
+                    help='how many batches to wait before logging training status')
+parser.add_argument('--model-dir', default='./model-mnist-smallCNN',
+                    help='directory of model for saving checkpoint')
+parser.add_argument('--save-freq', '-s', default=25, type=int, metavar='N',
+                    help='save frequency')
+parser.add_argument('--random',
+                    default=True,
+                    help='random initialization for PGD')
+parser.add_argument('--T', default=10,type=int,
+                    help='number of stages for wieght re initialization')
+parser.add_argument('--shrink', default=0.4, type=float
+                    help='shrink size for re initialization')
+parser.add_argument('--perturb', default=0.1, type=float
+                    help='shrink size for re initialization')
+args = parser.parse_args()
+
+# settings
+model_dir = args.model_dir
+if not os.path.exists(model_dir):
+    os.makedirs(model_dir)
+use_cuda = not args.no_cuda and torch.cuda.is_available()
+torch.manual_seed(args.seed)
+device = torch.device("cuda" if use_cuda else "cpu")
+kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+
+# setup data loader
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=True, download=True,
+                   transform=transforms.ToTensor()),
+    batch_size=args.batch_size, shuffle=True, **kwargs)
+
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=False,
+                   transform=transforms.ToTensor()),
+                   batch_size=args.test_batch_size, shuffle=False, **kwargs)
+
+
+def shrink_perturb(model, shrink= args.shrink, perturb=args.perturb):
+    # using a randomly-initialized model as a noise source respects how different kinds 
+    # of parameters are often initialized differently
+    new_init = SmallCNN().to(device)
+    # new_init = nn.DataParallel(new_init).cuda()
+
+    params1 = new_init.parameters()
+    params2 = model.parameters()
+    for p1, p2 in zip(*[params1, params2]):
+        p1.data = deepcopy(shrink * p2.data + perturb * p1.data)
+    return new_init
+
+def _pgd_whitebox(model,
+                  X,
+                  y,
+                  epsilon=args.epsilon,
+                  num_steps=args.attack_num_steps,
+                  step_size=args.step_size):
+    out = model(X)
+    err = (out.data.max(1)[1] != y.data).float().sum().item()
+    X_pgd = Variable(X.data, requires_grad=True)
+    if args.random:
+        random_noise = torch.FloatTensor(*X_pgd.shape).uniform_(-epsilon, epsilon).to(device)
+        X_pgd = Variable(X_pgd.data + random_noise, requires_grad=True)
+
+    for _ in range(num_steps):
+        opt = optim.SGD([X_pgd], lr=1e-3)
+        opt.zero_grad()
+
+        with torch.enable_grad():
+            loss = nn.CrossEntropyLoss()(model(X_pgd), y)
+        loss.backward()
+        eta = step_size * X_pgd.grad.data.sign()
+        X_pgd = Variable(X_pgd.data + eta, requires_grad=True)
+        eta = torch.clamp(X_pgd.data - X.data, -epsilon, epsilon)
+        X_pgd = Variable(X.data + eta, requires_grad=True)
+        X_pgd = Variable(torch.clamp(X_pgd, 0, 1.0), requires_grad=True)
+
+    output = model(X_pgd)
+    err_pgd = (output.data.max(1)[1] != y.data).float().sum().item()
+    adv_loss = F.cross_entropy(output, y, size_average=False).item()
+    #print('err pgd (white-box): ', err_pgd)
+    return err, err_pgd, adv_loss
+
+def eval_adv_test_whitebox(model, device, test_loader):
+    """
+    evaluate model by white-box attack
+    """
+    model.eval()
+    robust_err_total = 0
+    natural_err_total = 0
+    adv_loss = 0
+
+    for data, target in test_loader:
+        data, target = data.to(device), target.to(device)
+        # pgd attack
+        X, y = Variable(data, requires_grad=True), Variable(target)
+        err_natural, err_robust, batch_adv_loss = _pgd_whitebox(model, X, y)
+        robust_err_total += err_robust
+        natural_err_total += err_natural
+        adv_loss += batch_adv_loss
+    print('natural_err_total: ', natural_err_total)
+    print('robust_err_total: ', robust_err_total)
+    
+    adv_acc = 1 - (robust_err_total / len(test_loader.dataset))
+    adv_loss /= len(test_loader.dataset)
+                   
+    return adv_loss, adv_acc
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+
+        optimizer.zero_grad()
+
+        # calculate robust loss
+        loss = trades_loss(model=model,
+                           x_natural=data,
+                           y=target,
+                           optimizer=optimizer,
+                           step_size=args.step_size,
+                           epsilon=args.epsilon,
+                           perturb_steps=args.num_steps,
+                           beta=args.beta)
+
+        loss.backward()
+        optimizer.step()
+
+        # print progress
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+
+def eval_train(model, device, train_loader):
+    model.eval()
+    train_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in train_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            train_loss += F.cross_entropy(output, target, size_average=False).item()
+            pred = output.max(1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    train_loss /= len(train_loader.dataset)
+    print('Training: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+        train_loss, correct, len(train_loader.dataset),
+        100. * correct / len(train_loader.dataset)))
+    training_accuracy = correct / len(train_loader.dataset)
+    return train_loss, training_accuracy
+
+
+def eval_test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.cross_entropy(output, target, size_average=False).item()
+            pred = output.max(1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    test_loss /= len(test_loader.dataset)
+    print('Test: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    test_accuracy = correct / len(test_loader.dataset)
+    return test_loss, test_accuracy
+
+
+def adjust_learning_rate(optimizer, epoch):
+    """decrease the learning rate"""
+    lr = args.lr
+    if epoch >= 55:
+        lr = args.lr * 0.1
+    if epoch >= 75:
+        lr = args.lr * 0.01
+    if epoch >= 90:
+        lr = args.lr * 0.001
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def main():
+    # init model, Net() can be also used here for training
+    model = SmallCNN().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    train_losses, test_losses = [], []
+    train_accs, test_accs = [], []
+    adv_train_losses, adv_test_losses = [], []
+    adv_train_accs, adv_test_accs = [], []
+    P = int(args.epochs / args.T) #epochs per stage
+
+    for stage in range(args.T):
+        if stage == 0:
+            continue
+        else:
+            model = shrink_perturb(model)
+
+
+        for epoch in range(1, P + 1):
+            # adjust learning rate for SGD
+            real_epoch = stage*P + epoch
+            adjust_learning_rate(optimizer, real_epoch)
+            # adversarial training
+            train(args, model, device, train_loader, optimizer, real_epoch)
+            # evaluation on natural examples
+            print('================================================================')
+            train_loss, training_accuracy = eval_train(model, device, train_loader)
+            test_loss, test_accuracy = eval_test(model, device, test_loader)
+            print('================================================================')
+            adv_test_loss, adv_test_accuracy = eval_adv_test_whitebox(model, device, test_loader)
+            adv_train_loss, adv_training_accuracy = eval_adv_test_whitebox(model, device, train_loader)
+            print('================================================================')
+
+            print('ADV Test: Average loss: {:.4f}, Accuracy: {:.4f}%'.format(
+            adv_test_loss, 100. * adv_test_accuracy))
+
+            print('ADV Train: Average loss: {:.4f}, Accuracy: {:.4f}%'.format(
+            adv_train_loss, 100. * adv_training_accuracy))
+            print('================================================================')
+
+            train_losses.append(train_loss)
+            test_losses.append(test_loss)
+            train_accs.append(training_accuracy)
+            test_accs.append(test_accuracy)
+
+            adv_train_losses.append(adv_train_loss)
+            adv_test_losses.append(adv_test_loss)
+            adv_train_accs.append(adv_training_accuracy)
+            adv_test_accs.append(adv_test_accuracy)
+
+            if epoch % args.save_freq == 0:
+                torch.save(model.state_dict(),
+                        os.path.join(model_dir, 'model-nn-epoch{}.pt'.format(real_epoch)))
+                torch.save(optimizer.state_dict(),
+                        os.path.join(model_dir, 'opt-nn-checkpoint_epoch{}.tar'.format(real_epoch)))
+
+    
+    plt.plot(train_accs, label = 'train_acc')
+    plt.plot(test_accs, label = 'test_acc')
+    plt.legend()
+    plt.savefig('accs.png')
+    plt.clf()
+
+    plt.plot(adv_train_accs, label= 'train_acc_robust')
+    plt.plot(adv_test_accs, label = 'test_robust_accs')
+    plt.legend()
+    plt.savefig('adv accs.png')
+    plt.clf()
+
+    #plot the same for losses
+    
+    plt.plot(train_losses, label = 'train_loss')
+    plt.plot(test_losses, label = 'test_loss')
+    plt.legend()
+    plt.savefig('losses.png')
+    plt.clf()
+
+    plt.plot(adv_train_losses, label = 'train_loss_robust')
+    plt.plot(adv_test_losses, label = 'test_robust_loss')
+    plt.legend()
+    plt.savefig('adv losses.png')
+
+
+if __name__ == '__main__':
+    main()
